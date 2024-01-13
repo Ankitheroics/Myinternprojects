e3b80c6e963fe15bf9615e5ed7a55b910b88234e
Ankitheroics <98862728+Ankitheroics@users.noreply.github.com>
2024-01-13 23:12:13 +0530

    Add files via upload

---
+++ expense_tracker.py
@@ -0,0 +1,24 @@
+import sqlite3
+import datetime
+
+
+def Mymain():
+        print("**********************************************************")
+        print(" -=-=-= | WELCOME TO THE EXPENSE TRACKER SYSTEM | -=-=-=-")
+        print("===========================================================")
+        print("ENTER 1: ENTER YOUR THINGS WHICH YOU HAVE PURCHASED THE THING")
+        print("ENTER 2: ENTER YOUR DATE WHICH YOU HAVE BEEN PURCHASED THE THING")
+        print("ENTER 3: SEE YOUR DATA OVER HERE")
+        print("ENTER 4: YOUR DATA HAS BEEN SAVED WHEN WRITING PRODUCT")
+
+
+Mymain()
+
+
+P= self.__init__(self,root)
+
+print("RUPEES:")
+print("DATA HAS BEEN SAVED")
+
+
+
